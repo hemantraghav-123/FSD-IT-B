76a5dc2fe275dec51f831f119de0735341fd5d2f
hemantraghav-123 <hemant.23b0131263@abes.ac.in>
2025-09-29 12:31:53 +0530

    Add files via upload

---
+++ JS/jssync.js
@@ -0,0 +1,22 @@
+function register(){
+    console.log('register end');
+}
+function sendEmail(){
+    console.log('send email end');
+}
+function login(){
+    console.log('login end');
+}
+function getData(){
+    console.log('get data end');
+}
+function displayData(){
+    console.log('display Data End')
+}
+
+register();
+sendEmail();
+login();
+getData();
+displayData();
+console.log('call other application');
